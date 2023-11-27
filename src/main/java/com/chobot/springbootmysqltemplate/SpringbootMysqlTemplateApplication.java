52f701057d5248deda907fd85858421c6dae1beb
Joshua <chojoshua99@gmail.com>
2023-11-27 12:57:00 -0800

    first commit

---
+++ src/main/java/com/chobot/springbootmysqltemplate/SpringbootMysqlTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.chobot.springbootmysqltemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chobot/springbootmysqltemplate/SpringbootMysqlTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chobot.springbootmysqltemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
