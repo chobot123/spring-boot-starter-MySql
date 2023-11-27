3a6e0ac3b5fca69af0d1b1c103218789e986a39e
Joshua <chojoshua99@gmail.com>
2023-11-27 15:13:39 -0800

    add swagger json documentation url for endpoints

--- src/main/java/com/chobot/springbootmysqltemplate/SpringbootMysqlTemplateApplication.java
+++ src/main/java/com/chobot/springbootmysqltemplate/SpringbootMysqlTemplateApplication.java
@@ -3,6 +3,10 @@ package com.chobot.springbootmysqltemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * For Swagger Documentation for your endpoints: http://server:port/context-path/swagger-ui.html
+ */
+
 @SpringBootApplication
 public class SpringbootMysqlTemplateApplication {
 
